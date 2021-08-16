d23036838271c489801b47f6cff38483701128f3
Jan Guldentops <j@ba.be>
2021-08-16 19:15:00 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>PHP Test versie: Mon Aug 16 19:13:27 CEST 2021
+</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World - it was Mon Aug 16 19:13:27 CEST 2021
+ when this script was created</p>'; ?> 
+ </body>
+</html>
