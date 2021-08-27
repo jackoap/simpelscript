3b2b3a8d5db1b3e2b777887c1290a7ce01a6c5c5
Jan Guldentops <j@ba.be>
2021-08-27 02:28:00 +0200

    update 1630024080

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
  <head>
-  <title>PHP Test versie: Fri Aug 27 01:28:00 CEST 2021
+  <title>PHP Test versie: Fri Aug 27 02:28:00 CEST 2021
 </title>
  </head>
  <body>
- <?php echo '<p>Hello World - it was Fri Aug 27 01:28:00 CEST 2021
+ <?php echo '<p>Hello World - it was Fri Aug 27 02:28:00 CEST 2021
  when this script was created</p>'; ?> 
  </body>
 </html>
