79fe18e2ee96de692d67e24580dac4a3dec2f9bc
jackoap <44288378+jackoap@users.noreply.github.com>
2021-12-02 06:28:00 +0100

    update 1638422880

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
  <head>
-  <title>PHP Test versie: Thu Dec  2 05:28:00 CET 2021
+  <title>PHP Test versie: Thu Dec  2 06:28:00 CET 2021
 </title>
  </head>
  <body>
- <?php echo '<p>Hello World - it was Thu Dec  2 05:28:00 CET 2021
+ <?php echo '<p>Hello World - it was Thu Dec  2 06:28:00 CET 2021
  when this script was created</p>'; ?> 
  </body>
 </html>
