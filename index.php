01da4df6b29f83c8edb773fcdf353d9d424223f8
jackoap <44288378+jackoap@users.noreply.github.com>
2022-02-17 00:28:00 +0100

    update 1645054080

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
  <head>
-  <title>PHP Test versie: Wed Feb 16 23:28:01 CET 2022
+  <title>PHP Test versie: Thu Feb 17 00:28:00 CET 2022
 </title>
  </head>
  <body>
- <?php echo '<p>Hello World - it was Wed Feb 16 23:28:01 CET 2022
+ <?php echo '<p>Hello World - it was Thu Feb 17 00:28:00 CET 2022
  when this script was created</p>'; ?> 
  </body>
 </html>
